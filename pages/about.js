7197d142d07671f64bf96c9373ff9f8e6e8aa48a
Vincent <vincent.veri@gmail.com>
2022-09-28 18:05:28 +0200

    edit homepage

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About me</h2>
+            <div className="prose">
+                <p>I&apos;m an italian web developer, photographer and nerd.</p>
+                <p>I like solving Rubik&apos;s cube and reading tech articles. In my spare time I like writing about Photography and Web Technologies.</p>
+                <p>Sherlock Holmes is my favourite fictional character. I&apos;ve been studying Python for three years and I really love it.</p>
+            </div>
+        </div>
+    )
+}
