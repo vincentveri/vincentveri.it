b677cafb47b02692d1fdd68d89ad1e3d911b37ad
Vincent <vincent.veri@gmail.com>
2022-09-29 18:07:32 +0200

    integrate google analytics 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,50 @@
 import '../styles/globals.css';
-
+import Script from 'next/script'
+import { useEffect } from "react";
 import { ThemeProvider } from 'next-themes';
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+    
   return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        id="ga-tag"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="ga-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
+    </>
   );
 }
